9e058b28390499f7d50f6db1197a61baa4b7f17d
Sanjuro Jogdeo <junk@sanjuro.net>
2016-12-21 20:38:51 -0800

    Update date field to datetime and add published, and public fields

--- models.py
+++ models.py
@@ -5,9 +5,11 @@ from django.db import models
 class Post (models.Model):
     text = models.TextField()
     title = models.CharField(max_length=500, blank=True)
-    create_date = models.DateField()
-    update_date = models.DateField()
+    create_date = models.DateTimeField()
+    update_date = models.DateTimeField()
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    public = models.BooleanField(default=False)
+    published = models.BooleanField(default=False)
     
 
 class Tag (models.Model):
